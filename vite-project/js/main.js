16c7a9390ed9c92b08abb7d35d0fb8e0064b7da9
richie <113203265+R1chi333@users.noreply.github.com>
2022-11-19 20:48:56 -0500

    rujrj

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,1 +1,20 @@
 import "../styles/style.css";
+let audioElement = document.querySelector("#erhu");
+let canvasElement = document.querySelector("#waves");
+const wholePlanet = document.getElementById("planet-ring");
+import {Wave} from "@foobar404/wave";
+let wave = new Wave(audioElement, canvasElement);
+audioElement.volume = 0.2;
+
+wholePlanet.addEventListener("click", function(){
+    audioElement.play();
+    wave.addAnimation(new wave.animations.Lines({
+        count: 120,
+        lineColor: "white",
+    }));
+})
+
+const forwards = document.getElementById("forwards");
+forwards.addEventListener("click", function() {
+    console.log("fnjwekew");
+})
