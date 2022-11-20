7583e9ca5f461b005ae147f626f8ed34998274b4
richie <113203265+R1chi333@users.noreply.github.com>
2022-11-19 22:55:13 -0500

    ult

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -13,15 +13,46 @@ const audios = {
 }
 const DOMSelectors = {
     forwards: document.getElementById("forwards"),
-    backwards: document.getElementById("backwards")
+    backwards: document.getElementById("backwards"),
+    changeartist: document.getElementById("changeartist"),
+    starttext: document.getElementById("starttext"),
+    shopname: document.getElementById("shopname"),
+    shopcard: document.getElementById("shopcard"),
+    playCard: document.getElementById("play-card"),
+    playing: document.getElementById("nowPLaying"),
+    artist: document.getElementById("artist"),
 }
-wholePlanet.addEventListener("click", function(){
-    audios.sukida.play();
-    wave.addAnimation(new wave.animations.Lines({
-        count: 120,
-        lineColor: "white",
-    }));
-})
+const sfx = {
+    select: document.getElementById("select")
+}
+// wholePlanet.addEventListener("click", function(){
+//     audios.sukida.play();
+//     wave.addAnimation(new wave.animations.Lines({
+//         count: 120,
+//         lineColor: "white",
+//     }));
+// })
+let theme = 0
 DOMSelectors.forwards.addEventListener("click", function() {
-    console.log(songs);
+    sfx.select.play();
+    DOMSelectors.starttext.remove();
+    if (theme < 5) {
+        return theme ++
+    }
+    else{
+        return theme = 0
+    }
+})
+DOMSelectors.backwards.addEventListener("click", function() {
+    sfx.select.play();
+    DOMSelectors.starttext.remove();
+    if (theme > 0) {
+        return theme --
+    }
+    else{
+        return theme = 5
+    }
+})
+DOMSelectors.changeartist.addEventListener("click", function(){
+    console.log(theme)
 })
