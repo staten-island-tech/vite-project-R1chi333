f13bb96a649c6c67c528fc9ac66eac5189e02f37
richie <113203265+R1chi333@users.noreply.github.com>
2022-11-28 23:44:32 -0500

    uhjkn

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,73 +1,201 @@
 import "../styles/style.css";
 import { songs } from "./songs";
 import { Wave } from "@foobar404/wave";
+
 const DOMSelectors = {
-  deals: document.getElementById("deals"),
-  top2: document.getElementById("top2"),
-  logo: document.getElementById("logo"),
-  op: document.getElementById("op"),
-  menu: document.getElementById("menu"),
-  top: document.getElementById("top"),
-  colorchanger: document.getElementById("colorchanger"),
-  header: document.getElementById("header"),
-  drip: document.getElementById("drip"),
-  naall: document.getElementById("naall"),
-  colorwheel: document.getElementById("color-wheel"),
-  yoasobicard: document.getElementById("YOASOBI-AYASE"),
-  productlist: document.getElementById("productlist"),
-};
-addEventListener("scroll", function () {
-  const scrolled = window.scrollY;
-  if (scrolled >= 80) {
-    DOMSelectors.top.classList.add("up");
-    DOMSelectors.top.classList.remove("down");
-  }
-  if (scrolled < 80) {
-    DOMSelectors.top.classList.remove("up");
-    DOMSelectors.top.classList.add("down");
-  }
-});
+  ozu: document.getElementById("ozu"),
+  backTop: document.getElementById("backTop"),
+  title: document.getElementById("title"),
+  el: document.getElementById("el"),
+  back: document.getElementById("back")
+}
 
-DOMSelectors.logo.addEventListener("click", function () {
-  document.location.reload();
-});
+let audioElement = document.querySelector("#audio");
+let canvasElement = document.querySelector("#osuBase");
+let wave = new Wave(audioElement, canvasElement);
 
-let colortheme = 1;
-DOMSelectors.colorchanger.addEventListener("click", function () {
-  DOMSelectors.colorwheel.classList.add("spin");
+addEventListener("click", function(){
+  audioElement.play();
+})
+
+let open = false
+
+DOMSelectors.backTop.addEventListener("click", function(){
+  opening();
+})
+DOMSelectors.title.addEventListener("click", function(){
+  opening();
+})
+
+function addModeSelector(){
+  DOMSelectors.el.insertAdjacentHTML("afterend", 
+  `<div id="modes" class="hidden">
+  <div class="mode" id="easyMode">
+    <h2>EASY</h2>
+  </div>
+  <div class="mode" id="mediumMode">
+    <h2>MEDIUM</h2>
+  </div>
+  <div class="mode" id="hardMode">
+    <h2>HARD</h2>
+  </div>
+  <div class="mode" id="expertMode">
+    <h2>EXPERT</h2>
+</div>`)
+  const modes = document.getElementById("modes")
   setTimeout(() => {
-    DOMSelectors.colorwheel.classList.remove("spin");
-  }, "400");
-  colortheme++;
-  console.log(colortheme);
-  if (colortheme === 7) {
-    DOMSelectors.header.classList.remove("header6");
-    DOMSelectors.drip.classList.remove("drip6");
-    DOMSelectors.naall.classList.remove("bottomcolor6");
-    DOMSelectors.header.classList.add(`header1`);
-    DOMSelectors.drip.classList.add(`drip1`);
-    DOMSelectors.naall.classList.add(`bottomcolor1`);
-    colortheme = 1;
-    return colortheme;
-  }
-  DOMSelectors.header.classList.add(`header${colortheme}`);
-  DOMSelectors.drip.classList.add(`drip${colortheme}`);
-  DOMSelectors.naall.classList.add(`bottomcolor${colortheme}`);
-  DOMSelectors.header.classList.remove(`header${colortheme - 1}`);
-  DOMSelectors.drip.classList.remove(`drip${colortheme - 1}`);
-  DOMSelectors.naall.classList.remove(`bottomcolor${colortheme - 1}`);
-});
+    modes.classList.remove("hidden")
+    modes.classList.add("changeOpacity")
+    modes.classList.remove("changeOpacityBack")
+  }, "1")
+}
 
-DOMSelectors.yoasobicard.addEventListener("click", function () {
-  DOMSelectors.header.classList.add("changeOpacity");
-  DOMSelectors.drip.classList.add("changeOpacity");
-  DOMSelectors.productlist.classList.add("changeOpacity");
+function removeModeSelector(){
+  const modes = document.getElementById("modes")
+  modes.classList.remove("changeOpacity")
+  modes.classList.add("changeOpacityBack")
   setTimeout(() => {
-    DOMSelectors.header.remove();
-    DOMSelectors.drip.remove();
-    DOMSelectors.productlist.remove();
-  }, "1000");
-});
+    modes.remove();
+  }, "1000")
+}
+// function removeModeSelector(){
+//   const modes = modes
+//   modes.remove();
+//   DOMSelectors.backTop.classList.add("margin")
+//   DOMSelectors.backTop.classList.remove("newMargin")
+//   DOMSelectors.title.classList.add("margin")
+//   DOMSelectors.title.classList.remove("newMargin")
+//   DOMSelectors.back.classList.add("margin")
+//   DOMSelectors.back.classList.remove("newMargin")
+//   canvasElement.classList.remove("newMargin")
+//   canvasElement.classList.add("margin")
+// }
+function opening(){
+  if(open === false){
+    addModeSelector();
+    DOMSelectors.ozu.classList.remove("slideBack")
+    DOMSelectors.ozu.classList.add("slide")
+    open = true;
+    console.log(open)
+    return open;
+  } else{
+    removeModeSelector();
+    DOMSelectors.ozu.classList.add("slideBack")
+    DOMSelectors.ozu.classList.remove("slide")
+    open = false;
+    console.log(open)
+    return open;
+  }
+}
+
+//get DPI
+let dpi = window.devicePixelRatio;
+//get canvas
+let canvas = document.getElementById("osuBase");
+//get context
+let ctx = canvas.getContext("2d");
+function fix_dpi() {
+  //get CSS height
+  //the + prefix casts it to an integer
+  //the slice method gets rid of "px"
+  let style_height = +getComputedStyle(canvas)
+    .getPropertyValue("height")
+    .slice(0, -2);
+  //get CSS width
+ let style_width = +getComputedStyle(canvas)
+    .getPropertyValue("width")
+    .slice(0, -2);
+  //scale the canvas
+  canvas.setAttribute("height", style_height * dpi);
+  canvas.setAttribute("width", style_width * dpi);
+}
+fix_dpi();
+
+wave.addAnimation(new wave.animations.Glob({
+  count: 100,
+  fillColor: "pink",
+  lineWidth: 5,
+  lineColor: "#fff"
+}));
+
+
+
+
+
+
+// const DOMSelectors = {
+//   deals: document.getElementById("deals"),
+//   top2: document.getElementById("top2"),
+//   logo: document.getElementById("logo"),
+//   op: document.getElementById("op"),
+//   menu: document.getElementById("menu"),
+//   top: document.getElementById("top"),
+//   colorchanger: document.getElementById("colorchanger"),
+//   header: document.getElementById("header"),
+//   drip: document.getElementById("drip"),
+//   naall: document.getElementById("naall"),
+//   colorwheel: document.getElementById("color-wheel"),
+//   yoasobicard: document.getElementById("YOASOBI-AYASE"),
+//   productlist: document.getElementById("productlist"),
+// };
+// addEventListener("scroll", function () {
+//   const scrolled = window.scrollY;
+//   if (scrolled >= 80) {
+//     DOMSelectors.top.classList.add("up");
+//     DOMSelectors.top.classList.remove("down");
+//   }
+//   if (scrolled < 80) {
+//     DOMSelectors.top.classList.remove("up");
+//     DOMSelectors.top.classList.add("down");
+//   }
+// });
+
+// DOMSelectors.logo.addEventListener("click", function () {
+//   document.location.reload();
+// });
+
+// let colortheme = 1;
+// DOMSelectors.colorchanger.addEventListener("click", function () {
+//   DOMSelectors.colorwheel.classList.add("spin");
+//   setTimeout(() => {
+//     DOMSelectors.colorwheel.classList.remove("spin");
+//   }, "400");
+//   colortheme++;
+//   console.log(colortheme);
+//   if (colortheme === 7) {
+//     DOMSelectors.header.classList.remove("header6");
+//     DOMSelectors.drip.classList.remove("drip6");
+//     DOMSelectors.naall.classList.remove("bottomcolor6");
+//     DOMSelectors.header.classList.add(`header1`);
+//     DOMSelectors.drip.classList.add(`drip1`);
+//     DOMSelectors.naall.classList.add(`bottomcolor1`);
+//     colortheme = 1;
+//     return colortheme;
+//   }
+//   DOMSelectors.header.classList.add(`header${colortheme}`);
+//   DOMSelectors.drip.classList.add(`drip${colortheme}`);
+//   DOMSelectors.naall.classList.add(`bottomcolor${colortheme}`);
+//   DOMSelectors.header.classList.remove(`header${colortheme - 1}`);
+//   DOMSelectors.drip.classList.remove(`drip${colortheme - 1}`);
+//   DOMSelectors.naall.classList.remove(`bottomcolor${colortheme - 1}`);
+// });
+
+// DOMSelectors.yoasobicard.addEventListener("click", function () {
+//   DOMSelectors.header.classList.add("changeOpacity");
+//   DOMSelectors.drip.classList.add("changeOpacity");
+//   DOMSelectors.productlist.classList.add("changeOpacity");
+//   setTimeout(() => {
+//     DOMSelectors.header.remove();
+//     DOMSelectors.drip.remove();
+//     DOMSelectors.productlist.remove();
+//   }, "1000");
+// });
+
+
+
+
+
+
 
 // const DOMSelectors = {
 //   forwards: document.getElementById("forwards"),
